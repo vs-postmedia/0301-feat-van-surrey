b1a11aa7515e7f1494337ae7275e9623eed3bf43
Nathan Griffiths <ngriffiths@postmedia.com>
2025-02-27 16:23:51 -0800

    minor edits

--- src/App.svelte
+++ src/App.svelte
@@ -56,13 +56,6 @@
     <Steps bind:currentStep={currentStep}/>
 </ul>
 
-<footer>
-    <p class="source">Source:
-    </p>
-    <!-- flourish logo -->
-    <div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/3706064/?utm_source=embed&utm_campaign=visualisation/3706064' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
-</footer>
-
 <!-- CSS -->
 <style>
     /* STEPS */
